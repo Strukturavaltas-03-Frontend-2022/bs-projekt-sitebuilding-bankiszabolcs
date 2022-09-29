701fd92cbedc8d8602daca1e7493c73600993a72
szabi1112 <banki.szabolcs@gmail.com>
2022-09-29 23:03:00 +0200

    alakul

---
+++ main.js
@@ -0,0 +1,29 @@
+$('#privacyModal').on('shown.bs.modal', function () {
+    $('#privacy').trigger('focus')
+  })
+
+  $('#faqModal').on('shown.bs.modal', function () {
+    $('#faq').trigger('focus')
+  })
+
+  $('#termsModal').on('shown.bs.modal', function () {
+    $('#terms').trigger('focus')
+  })
+
+  const navbar = document.querySelector('#menu')
+  const startMenuItem = document.querySelector('.start')
+  const menuItems = Array.from(document.querySelectorAll('.menu--item'))
+  document.addEventListener('scroll', ()=>{
+    if(window.pageYOffset>0){
+        navbar.style.background = 'white'
+        startMenuItem.style.color = 'grey';
+        menuItems.map(element => element.style.color = 'grey') 
+      }else{
+        navbar.style.background = 'unset'
+        startMenuItem.style.color = 'var(--yellow)' 
+        menuItems.map(element => element.style.color = '#e1c4d6') 
+      }
+
+  })
+
+
