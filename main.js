5d02e336f47c76424732cd4f106503ec55b086b9
szabi1112 <banki.szabolcs@gmail.com>
2022-09-30 20:53:32 +0200

    mask

--- main.js
+++ main.js
@@ -10,19 +10,32 @@ $('#privacyModal').on('shown.bs.modal', function () {
     $('#terms').trigger('focus')
   })
 
+
+
+
   const navbar = document.querySelector('#menu')
   const startMenuItem = document.querySelector('.start')
   const menuItems = Array.from(document.querySelectorAll('.menu--item'))
   document.addEventListener('scroll', ()=>{
     if(window.pageYOffset>0){
         navbar.style.background = 'white'
-        startMenuItem.style.color = 'grey';
-        menuItems.map(element => element.style.color = 'grey')
+        startMenuItem.classList.add('start--sticky')
+        startMenuItem.classList.remove('start')
+        menuItems.map(element => {
+          element.classList.add('menu__item--sticky')
+          element.classList.remove('menu--item')
+        })
+
       }else{
         navbar.style.background = 'unset'
-        startMenuItem.style.color = 'var(--yellow)' 
-        menuItems.map(element => element.style.color = '#e1c4d6') 
+        startMenuItem.classList.remove('start--sticky')
+        startMenuItem.classList.add('start')
+        menuItems.map(element => {
+          element.classList.remove('menu__item--sticky')
+          element.classList.add('menu--item')
+        })
       }
 
   })
+
 
